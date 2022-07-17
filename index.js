975edb6798bc8644996f5a1384c81baab03bf408
Alvaro Vega <alvaro.vega@automattic.com>
2022-07-17 18:56:05 +0200

    add functionality and prepare for prod serv

--- index.js
+++ index.js
@@ -5,33 +5,70 @@ const axios = require("axios");
 
 const { TOKEN, SERVER_URL, PORT } = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
-console.log(TELEGRAM_API)
-const URI = `/webhook/${TOKEN}`
-const WEBHOOK_URL = SERVER_URL + URI
+const URI = `/webhook/${TOKEN}`;
+const WEBHOOK_URL = SERVER_URL + URI;
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
 const init = async () => {
-    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
-    console.log(res.data)
-}
+  const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
+  console.log(res.data);
+};
 
-app.post(URI, async(req, res) => {
-    console.log(req.body)
+const reverseText = (text) => {
+  return text.split("").reverse().join("");
+};
+app.post(URI, async (req, res) => {
+  console.log(req.body);
 
-    const chatId = req.body.message.chat.id
-    const text = req.body.message.text
+  let chatId = req.body.message.chat.id;
+  let text = req.body.message.text;
+  console.log(text);
+  await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    chat_id: chatId,
+    text: reverseText(text)
+  });
+  res.send();
+});
 
-    await axios.post(`${TELEGRAM_API}/sendMessage`,{
-        chat_id: chatId,
-        text: text
-    })
-})
+app.listen(process.env.PORT || 8080, async () => {
+  await init();
+});
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
+const { TOKEN, SERVER_URL, PORT } = process.env;
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/webhook/${TOKEN}`;
+const WEBHOOK_URL = SERVER_URL + URI;
 
-app.listen(process.env.PORT || 8080, async() => {
-    console.log( "the pro enn port is" + process.env.PORT )
-    console.log( "the app is running int port", process.env.PORT || 8080 )
-    await init()
-})
+const app = express();
+app.use(bodyParser.json());
+
+const init = async () => {
+  const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
+  console.log(res.data);
+};
+
+const reverseText = (text) => {
+  return text.split("").reverse().join("");
+};
+app.post(URI, async (req, res) => {
+  console.log(req.body);
+
+  let chatId = req.body.message.chat.id;
+  let text = req.body.message.text;
+  console.log(text);
+  await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    chat_id: chatId,
+    text: reverseText(text)
+  });
+  res.send();
+});
+
+app.listen(process.env.PORT || 8080, async () => {
+  await init();
+});
