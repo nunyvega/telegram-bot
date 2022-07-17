4fb31c04cbaf96ee7c5f91c8b73c78a2a9966adc
Alvaro Vega <alvaro.vega@automattic.com>
2022-07-17 20:44:27 +0200

    add logic to interact and send memes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const memes = require("random-memes");
 
 const { TOKEN, SERVER_URL, PORT } = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
@@ -13,62 +14,32 @@ app.use(bodyParser.json());
 
 const init = async () => {
   const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
-  console.log(res.data);
 };
 
-const reverseText = (text) => {
-  return text.split("").reverse().join("");
-};
 app.post(URI, async (req, res) => {
-  console.log(req.body);
-
   let chatId = req.body.message.chat.id;
-  let text = req.body.message.text;
-  console.log(text);
-  await axios.post(`${TELEGRAM_API}/sendMessage`, {
+  //let text = req.body.message.text;
+  let meme = await memes.random();
+
+  await axios.post(`${TELEGRAM_API}/sendPhoto`, {
     chat_id: chatId,
-    text: reverseText(text)
+    photo: meme.image,
+    caption: meme.caption
   });
+  await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    chat_id: 788243822,
+    text: "meme sent"
+  });
+  // Notify the API that the message was recieved
   res.send();
 });
 
+//listen for webhook notifications
 app.listen(process.env.PORT || 8080, async () => {
   await init();
 });
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
 
-const { TOKEN, SERVER_URL, PORT } = process.env;
-const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
-const URI = `/webhook/${TOKEN}`;
-const WEBHOOK_URL = SERVER_URL + URI;
-
-const app = express();
-app.use(bodyParser.json());
-
-const init = async () => {
-  const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
-  console.log(res.data);
-};
-
-const reverseText = (text) => {
+// Old functionality to reverse text
+/*const reverseText = (text) => {
   return text.split("").reverse().join("");
-};
-app.post(URI, async (req, res) => {
-  console.log(req.body);
-
-  let chatId = req.body.message.chat.id;
-  let text = req.body.message.text;
-  console.log(text);
-  await axios.post(`${TELEGRAM_API}/sendMessage`, {
-    chat_id: chatId,
-    text: reverseText(text)
-  });
-  res.send();
-});
-
-app.listen(process.env.PORT || 8080, async () => {
-  await init();
-});
+};*/
